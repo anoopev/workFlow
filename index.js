10fc6560bd7ea6a268e302297d06e074cb3839b2
anoopev <blocksnoop5@gmail.com>
2018-11-02 18:26:50 +0530

    updates to deploy to heroku

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const PORT = 3000;
 const { mongoose } = require('./db.js');
 var employeeController = require('./controllers/employeeController.js');
 var workorderController = require('./controllers/workorderController.js');
+const path = require('path');
+
 
 var app = express();
+app.use(express.static(__dirname+'/dist/ngApp3'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/ngApp3/index.html'));
+});
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.listen(PORT, () => console.log('Server started at: ' + PORT));
 
 app.use('/employees', employeeController);
-app.use('/workorders', workorderController)
+app.use('/workorders', workorderController);
+app.listen(process.env.PORT || 8080);
